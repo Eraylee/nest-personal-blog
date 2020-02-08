3cb02c8d9e39d1bec8c8fd40c2b1a67ec45fefa2
eraylee <599023328@qq.com>
2020-02-08 11:01:27 +0800

    修改ip获取

--- src/feature/comment/comment.controller.ts
+++ src/feature/comment/comment.controller.ts
@@ -2,7 +2,7 @@
  * @Author: ERAYLEE
  * @Date: 2020-01-16 17:22:25
  * @LastEditors  : ERAYLEE
- * @LastEditTime : 2020-02-08 10:58:07
+ * @LastEditTime : 2020-02-08 11:01:19
  */
 import {
   Get,
@@ -63,6 +63,7 @@ export class CommentController extends BaseController<CommentEntity> {
   @Post()
   async createComment(@Body() comment: CreateCommentDto, @Req() req: Request) {
     const ip = req.headers['x-real-ip'] || req['connection'].remoteAddress;
+    console.log(req.headers);
     const agent = req.headers['user-agent'];
     return {
       code: 200,
